88a0857d5187b3e80ee249d4d360c9c4162c02d8
Mindexperiment <andrewloyd@libero.it>
2019-02-04 17:12:40 +0100

    add preface background

--- resources/views/website/preface.blade.php
+++ resources/views/website/preface.blade.php
@@ -6,4 +6,14 @@
    <h2 class="subtitle is-3">{{ __( 'website.preface.whoami' ) }}</h2>
   </div>
  </div>
+
+ <div class="w-100 position-absolute right-0 bottom-0 left-0 z-index-n1">
+  <figure class="ie-irregular-shape-2-bottom">
+   <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1920 323" enable-background="new 0 0 1920 323" xml:space="preserve">
+    <style type="text/css">.irregular-shape-3-0{fill:#377DFF;}</style>
+    <polygon class="irregular-shape-3-0 fill-primary" opacity=".125" points="0,323 1920,323 1920,0 "></polygon>
+    <polygon class="irregular-shape-3-0 fill-primary" opacity=".085" points="-0.5,322.5 -0.5,131.5 658.3,212.3 "></polygon>
+   </svg>
+  </figure>
+ </div>
 </section>
