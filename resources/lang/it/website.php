5a4eea27aa26d395c07e063a14e6e1dfb025bd93
Mindexperiment <andrewloyd@libero.it>
2019-02-04 11:29:01 +0100

    italian translation

---
+++ resources/lang/it/website.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+
+ /*
+ |--------------------------------------------------------------------------
+ | Website Language Lines
+ |--------------------------------------------------------------------------
+ */
+
+ 'copyright' => "Tutti i diritti riservati",
+ 'header' => [
+  'menu' => [
+   'skills'    => "abilità",
+   'portfolio' => "portfolio",
+   'contacts'  => "contatti"
+  ]
+ ],
+ 'skills' => [
+  'title'       => "Abilità",
+  'lead'        => "Più di 10 anni di esperienza come sviluppatore full-stack. Ho iniziato verso la fine del 2008, per passione, scrivendo piccole applicazioni web. Sono cresciuto grazie allo studio e alle molteplici collaborazioni con aziende IT italiane. Ora, ho acquisito tutte le abilità necessarie per la gestione di progetti di piccole o grandi dimensioni.",
+  'fsd'         => "Sviluppatore Full Stack",
+  'passion'     => "Sono uno sviluppatore Full Stack, Amo approfondire tutti i segreti della rete e delle applicazioni web.",
+  'commonlang'  => "Linguaggi Comuni:",
+  'speciallang' => "Linguaggi Speciali:",
+  'tools'       => "Strumenti di Sviluppo:",
+  'frameworks'  => "Frameworks"
+ ],
+ 'portfolio' => [
+  'button' => "Visita il Sito"
+ ],
+ 'contacts' => [
+  'welcome' => "Ciao, sono Andrea. Felice di conoscerti.",
+  'lead'    => "Progetta la tua Web-app completa con una solida esperienza al tuo fianco.",
+  'contact' => "Mettiamoci in contatto:"
+ ],
+ 'preface' => [
+  'welcome' => "Benvenuto",
+  'whoami'  => "Sono uno sviluppatore Full Stack"
+ ]
+];
