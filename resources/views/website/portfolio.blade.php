fe4de268d7aebec0ddb1adfec002039341e35f98
Mindexperiment <andrewloyd@libero.it>
2019-02-04 18:33:08 +0100

    add portfolio images src

--- resources/views/website/portfolio.blade.php
+++ resources/views/website/portfolio.blade.php
@@ -6,7 +6,7 @@
 
     <div class="column is-12-mobile is-half-tablet is-one-third-desktop">
      <figure class="image is-3by2">
-      <img src="" class="project-thumb" />
+      <img src="{{ asset( 'images/layout/cmigea.jpg' ) }}" class="project-thumb" />
       <figcaption>
        <h1 class="title is-size-5 is-size-4-widescreen">Centro Medico IGEA</h1>
        <a href="http://www.centromedigea.it" class="button is-primary is-outlined is-rounded" target="_blank">
@@ -20,7 +20,7 @@
 
     <div class="column is-12-mobile is-half-tablet is-one-third-desktop">
      <figure class="image is-3by2">
-      <img src="" class="project-thumb" />
+      <img src="{{ asset( 'images/layout/sartori.jpg' ) }}" class="project-thumb" />
       <figcaption>
        <h1 class="title is-size-5 is-size-4-widescreen">Sartori Video su Misura</h1>
        <a href="https://www.sartorisilvia.com" class="button is-primary is-outlined is-rounded" target="_blank">
@@ -34,7 +34,7 @@
 
     <div class="column is-12-mobile is-half-tablet is-one-third-desktop">
      <figure class="image is-3by2">
-      <img src="" class="project-thumb" />
+      <img src="{{ asset( 'images/layout/kaleskop.jpg' ) }}" class="project-thumb" />
       <figcaption>
        <h1 class="title is-size-5 is-size-4-widescreen">Kaleskop Advertisement</h1>
        <a href="https://kaleskop.com" class="button is-primary is-outlined is-rounded" target="_blank">
