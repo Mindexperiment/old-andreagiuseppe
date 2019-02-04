22b0e3a874baf3bcb8dd55f460a6e951f050690c
Mindexperiment <andrewloyd@libero.it>
2019-02-04 11:10:59 +0100

    correct format on language

--- resources/lang/en/website.php
+++ resources/lang/en/website.php
@@ -11,31 +11,31 @@ return [
  'copyright' => "All right reserved",
  'header' => [
   'menu' => [
-   'skills' => "skills",
+   'skills'    => "skills",
    'portfolio' => "portfolio",
-   'contacts' => "contacts"
+   'contacts'  => "contacts"
   ]
  ],
  'preface' => [
   'welcome' => "Welcome",
-  'whoami' => "I'm a Full-Stack developer"
+  'whoami'  => "I'm a Full-Stack developer"
  ],
  'skills' => [
-  'title' => "Skills",
-  'lead' => "Over 10 years of experience as a full-stack developer. I started towards the end of 2008 coding small applications as a hobby. I grew up thanks to the study and many collaborations with IT companies. Now, I acquired all the skills needed to manage small to large-sized projects.",
-  'fsd' => "Full Stack Developer",
-  'passion' => "I'm a Full-Stack Developer, I love diggin deeper inside all secrets of network and web application.",
-  'commonlang' => "Common Languages I Speak:",
+  'title'       => "Skills",
+  'lead'        => "Over 10 years of experience as a full-stack developer. I started towards the end of 2008 coding small applications as a hobby. I grew up thanks to the study and many collaborations with IT companies. Now, I acquired all the skills needed to manage small to large-sized projects.",
+  'fsd'         => "Full Stack Developer",
+  'passion'     => "I'm a Full-Stack Developer, I love diggin deeper inside all secrets of network and web application.",
+  'commonlang'  => "Common Languages I Speak:",
   'speciallang' => "Special Languages I Speak:",
-  'tools' => "Development Tools:",
-  'frameworks' => "Frameworks"
+  'tools'       => "Development Tools:",
+  'frameworks'  => "Frameworks"
  ],
  'portfolio' => [
   'button' => "Visit Website"
  ],
  'about' => [
   'welcome' => "Hi, I'm Andrea. Nice to meet you.",
-  'lead' => "Design your full-fledged web apps with a solid expertise beside you.",
+  'lead'    => "Design your full-fledged web apps with a solid expertise beside you.",
   'contact' => "Let's get in touch with me:"
  ]
 ];
