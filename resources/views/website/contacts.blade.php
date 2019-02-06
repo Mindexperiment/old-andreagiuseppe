2f2ed66cec2138acf13571cce8d717ec8b76384f
Mindexperiment <andrewloyd@libero.it>
2019-02-06 19:43:06 +0100

    fixed phone number

--- resources/views/website/contacts.blade.php
+++ resources/views/website/contacts.blade.php
@@ -13,7 +13,7 @@
      <h4 class="title is-5">{{ __( 'website.contacts.welcome' ) }}</h4>
      <p>{{ __( 'website.contacts.lead' ) }}</p>
      <p>{{ __( 'website.contacts.email' ) }} <a href="mailto:andrewloyd@libero.it">andrewloyd{at}libero.it</a></p>
-     <p>{{ __( 'website.contacts.phone' ) }} <a href="tel:+393451607628">+39 345 1607628</a></p>
+     <p>{{ __( 'website.contacts.phone' ) }} <a href="tel:+393920460819">+39 392 0460819</a></p>
     </div>
    </div>
   </article>
