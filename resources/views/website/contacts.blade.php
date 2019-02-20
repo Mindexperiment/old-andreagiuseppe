2e62ba2831a07d68745bf35f39a21ac084ee16a3
Mindexperiment <andrewloyd@libero.it>
2019-02-20 15:37:25 +0100

    style contact section

--- resources/views/website/contacts.blade.php
+++ resources/views/website/contacts.blade.php
@@ -1,20 +1,16 @@
-<section id="contacts">
- <div>
+<section id="contacts h-64">
+ <div class="container mx-auto text-center">
 
-  <article>
+  <article class="flex items-start">
    <figure>
-    <div>
-     <img src="" />
-    </div>
+    <img src="" class="max-w-auto h-auto w-1/4 rounded shadow" />
    </figure>
 
-   <div>
-    <div>
-     <h4>{{ __( 'website.contacts.welcome' ) }}</h4>
-     <p>{{ __( 'website.contacts.lead' ) }}</p>
-     <p>{{ __( 'website.contacts.email' ) }} <a href="mailto:andrewloyd@libero.it">andrewloyd{at}libero.it</a></p>
-     <p>{{ __( 'website.contacts.phone' ) }} <a href="tel:+393920460819">+39 392 0460819</a></p>
-    </div>
+   <div class="flex-1">
+    <h4 class="text-2xl font-medium">{{ __( 'website.contacts.welcome' ) }}</h4>
+    <p>{{ __( 'website.contacts.lead' ) }}</p>
+    <p>{{ __( 'website.contacts.email' ) }} <a href="mailto:andrewloyd@libero.it">andrewloyd{at}libero.it</a></p>
+    <p>{{ __( 'website.contacts.phone' ) }} <a href="tel:+393920460819">+39 392 0460819</a></p>
    </div>
   </article>
 
