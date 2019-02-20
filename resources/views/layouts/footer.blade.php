bdffffca63a8e004ab9608a3f78ee021fb89bac3
Mindexperiment <andrewloyd@libero.it>
2019-02-20 15:48:05 +0100

    footer section

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,14 +1,12 @@
-<footer>
- <div>
-  <div>
-   <div>
-    <address>
-     <p>Andrea Giuseppe Pretto</p>
-     <p>Via A. Manzoni, 6</p>
-     <p>36073 Cornedo Vicentino (Vi)</p>
-     <p>P.Iva 03458890245</p>
-    </address>
-   </div>
+<footer class="pt-16 bg-grey-light">
+ <div class="container mx-auto">
+  <div class="flex justify-between">
+   <address>
+    <p class="text-xl font-medium">Andrea Giuseppe Pretto</p>
+    <p>Via A. Manzoni, 6</p>
+    <p>36073 Cornedo Vicentino (Vi)</p>
+    <p>P.Iva 03458890245</p>
+   </address>
 
    <div>
     <p>Copyright &copy; andreagiuseppe.com</p>
