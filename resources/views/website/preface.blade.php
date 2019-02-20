1a0b034454d25b04be6404dfc08fcb70cb9a85b8
Mindexperiment <andrewloyd@libero.it>
2019-02-20 14:58:57 +0100

    improve skills

--- resources/views/website/preface.blade.php
+++ resources/views/website/preface.blade.php
@@ -3,6 +3,6 @@
   <h3 class="text-2xl font-light">{{ __( 'website.preface.welcome' ) }}</h3>
   <h1 class="text-4xl font-bold">Andrea Giuseppe</h1>
   <h2 class="text-3xl font-medium">{{ __( 'website.preface.whoami' ) }}</h2>
-  <h4 class="text-xl">{{ __( 'website.preface.plus' ) }}</h4>
+  <h4 class="text-xl font-light">{{ __( 'website.preface.plus' ) }}</h4>
  </div>
 </section>
--- resources/views/website/skills.blade.php
+++ resources/views/website/skills.blade.php
@@ -10,24 +10,24 @@
    </div>
 
    <div class="w-1/2 p-4">
-    <div>
+    <div class="p-6 text-center bg-white">
      <article>
       <div>
        <div>
         <span role="img" aria-label="emoji">👨‍💻</span>
-        <h2>{{ __( 'website.skills.fsd' ) }}</h2>
+        <h2 class="text-3xl font-light">{{ __( 'website.skills.fsd' ) }}</h2>
         <p>{{ __( 'website.skills.passion' ) }}</p>
 
-        <div>
-         <h4>{{ __( 'website.skills.commonlang' ) }}</h4>
-         <div>
+        <div class="text-left">
+         <h4 class="text-3xl font-medium">{{ __( 'website.skills.commonlang' ) }}</h4>
+         <div class="flex">
           <span>{{ __( "Italian" ) }}</span>
           <span>{{ __( "English" ) }}</span>
           <span>{{ __( "French" ) }}</span>
          </div>
 
-         <h4>{{ __( 'website.skills.speciallang' ) }}</h4>
-         <div>
+         <h4 class="text-3xl font-medium">{{ __( 'website.skills.speciallang' ) }}</h4>
+         <div class="flex">
           <span>HTML5</span>
           <span>CSS3/SASS</span>
           <span>JS (ES6)</span>
@@ -35,8 +35,8 @@
           <span>MySql 8</span>
          </div>
 
-         <h4>{{ __( 'website.skills.tools' ) }}</h4>
-         <div>
+         <h4 class="text-3xl font-medium">{{ __( 'website.skills.tools' ) }}</h4>
+         <div class="flex">
           <span>AWS Network</span>
           <span>GIT</span>
           <span>NPM</span>
@@ -45,8 +45,8 @@
           <span>G-Suite</span>
          </div>
 
-         <h4>{{ __( 'website.skills.frameworks' ) }}</h4>
-         <div>
+         <h4 class="text-3xl font-medium">{{ __( 'website.skills.frameworks' ) }}</h4>
+         <div class="flex">
           <span>Laravel</span>
           <span>JQuery</span>
           <span>Vue</span>
