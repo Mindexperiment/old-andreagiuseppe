d852715143a52c278dd2683a239885090b9a3692
Mindexperiment <andrewloyd@libero.it>
2019-02-20 14:40:32 +0100

    add container to header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,22 +1,24 @@
-<header class="flex items-center justify-between">
- <div class="flex-initial mr-6">
-  <a href="{{ route( 'homepage' ) }}" >Andrea Giuseppe</a>
-  <div><span></span><span></span><span></span></div>
+<header>
+ <div class="container mx-auto flex items-center justify-between">
+  <div class="flex-initial mr-6">
+   <a href="{{ route( 'homepage' ) }}" >Andrea Giuseppe</a>
+   <div><span></span><span></span><span></span></div>
+  </div>
+
+  <nav>
+   <ul class="list-reset flex justify-between">
+    <li class="mr-3">
+     <a href="#skills">{{ __( "website.header.menu.skills" ) }}</a>
+    </li>
+
+    <li class="mr-3">
+     <a href="#portfolio">{{ __( "website.header.menu.portfolio" ) }}</a>
+    </li>
+
+    <li>
+     <a href="#contacts">{{ __( "website.header.menu.contacts" ) }}</a>
+    </li>
+   </ul>
+  </nav>
  </div>
-
- <nav>
-  <ul class="list-reset flex justify-between">
-   <li class="mr-3">
-    <a href="#skills">{{ __( "website.header.menu.skills" ) }}</a>
-   </li>
-
-   <li class="mr-3">
-    <a href="#portfolio">{{ __( "website.header.menu.portfolio" ) }}</a>
-   </li>
-
-   <li class="mr-3">
-    <a href="#contacts">{{ __( "website.header.menu.contacts" ) }}</a>
-   </li>
-  </ul>
- </nav>
 </header>
