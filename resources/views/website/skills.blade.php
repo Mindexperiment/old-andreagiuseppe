bbd8316f32cb03aea826507eff5796c9e47cd0c7
Mindexperiment <andrewloyd@libero.it>
2019-02-20 15:14:19 +0100

    improve skills

--- resources/views/website/skills.blade.php
+++ resources/views/website/skills.blade.php
@@ -3,10 +3,8 @@
   <div class="flex">
 
    <div class="w-1/2 p-4">
-    <h1>
-     <span>{{ __( 'website.skills.title' ) }}</span>
-    </h1>
-    <p>{{ __( 'website.skills.lead' ) }}</p>
+    <h1 class="mb-4 text-4xl font-light">{{ __( 'website.skills.title' ) }}</h1>
+    <p class="tracking-wide leading-tight">{{ __( 'website.skills.lead' ) }}</p>
    </div>
 
    <div class="w-1/2 p-4">
@@ -15,42 +13,42 @@
       <div>
        <div>
         <span role="img" aria-label="emoji">👨‍💻</span>
-        <h2 class="text-3xl font-light">{{ __( 'website.skills.fsd' ) }}</h2>
+        <h2 class="mb-4 text-3xl font-light">{{ __( 'website.skills.fsd' ) }}</h2>
         <p>{{ __( 'website.skills.passion' ) }}</p>
 
         <div class="text-left">
-         <h4 class="text-3xl font-medium">{{ __( 'website.skills.commonlang' ) }}</h4>
+         <h4 class="mb-4 text-3xl font-medium">{{ __( 'website.skills.commonlang' ) }}</h4>
          <div class="flex justify-around">
-          <span class="p-4 bg-yellow-lightest text-yellow-light rounded shadow">{{ __( "Italian" ) }}</span>
-          <span class="p-4 bg-yellow-lightest text-yellow-light rounded shadow">{{ __( "English" ) }}</span>
-          <span class="p-4 bg-yellow-lightest text-yellow-light rounded shadow">{{ __( "French" ) }}</span>
+          <span class="p-3 bg-yellow-lightest text-yellow rounded shadow">{{ __( "Italian" ) }}</span>
+          <span class="p-3 bg-yellow-lightest text-yellow rounded shadow">{{ __( "English" ) }}</span>
+          <span class="p-3 bg-yellow-lightest text-yellow rounded shadow">{{ __( "French" ) }}</span>
          </div>
 
-         <h4 class="text-xl font-medium">{{ __( 'website.skills.speciallang' ) }}</h4>
-         <div class="flex justify-around">
-          <span class="p-4 bg-green-lightest text-green-light rounded shadow">HTML5</span>
-          <span class="p-4 bg-green-lightest text-green-light rounded shadow">CSS3/SASS</span>
-          <span class="p-4 bg-green-lightest text-green-light rounded shadow">JS (ES6)</span>
-          <span class="p-4 bg-green-lightest text-green-light rounded shadow">PHP 7.2</span>
-          <span class="p-4 bg-green-lightest text-green-light rounded shadow">MySql 8</span>
-         </div>
-
-         <h4 class="text-xl font-medium">{{ __( 'website.skills.tools' ) }}</h4>
+         <h4 class="mb-4 text-xl font-medium">{{ __( 'website.skills.speciallang' ) }}</h4>
          <div class="flex flex-wrap justify-around">
-          <span class="p-4 bg-teal-lightest text-teal-light rounded shadow">AWS Network</span>
-          <span class="p-4 bg-teal-lightest text-teal-light rounded shadow">GIT</span>
-          <span class="p-4 bg-teal-lightest text-teal-light rounded shadow">NPM</span>
-          <span class="p-4 bg-teal-lightest text-teal-light rounded shadow">WEBPACK</span>
-          <span class="p-4 bg-teal-lightest text-teal-light rounded shadow">VSCODE</span>
-          <span class="p-4 bg-teal-lightest text-teal-light rounded shadow">G-Suite</span>
+          <span class="p-3 bg-green-lightest text-green rounded shadow">HTML5</span>
+          <span class="p-3 bg-green-lightest text-green rounded shadow">CSS3/SASS</span>
+          <span class="p-3 bg-green-lightest text-green rounded shadow">JS (ES6)</span>
+          <span class="p-3 bg-green-lightest text-green rounded shadow">PHP 7.2</span>
+          <span class="p-3 bg-green-lightest text-green rounded shadow">MySql 8</span>
          </div>
 
-         <h4 class="text-xl font-medium">{{ __( 'website.skills.frameworks' ) }}</h4>
-         <div class="flex">
-          <span class="p-4 bg-red-lightest text-red-light rounded shadow">Laravel</span>
-          <span class="p-4 bg-red-lightest text-red-light rounded shadow">JQuery</span>
-          <span class="p-4 bg-red-lightest text-red-light rounded shadow">Vue</span>
-          <span class="p-4 bg-red-lightest text-red-light rounded shadow">Bootstrap/Bulma</span>
+         <h4 class="mb-4 text-xl font-medium">{{ __( 'website.skills.tools' ) }}</h4>
+         <div class="flex flex-wrap justify-around">
+          <span class="p-3 bg-teal-lightest text-teal rounded shadow">AWS Network</span>
+          <span class="p-3 bg-teal-lightest text-teal rounded shadow">GIT</span>
+          <span class="p-3 bg-teal-lightest text-teal rounded shadow">NPM</span>
+          <span class="p-3 bg-teal-lightest text-teal rounded shadow">WEBPACK</span>
+          <span class="p-3 bg-teal-lightest text-teal rounded shadow">VSCODE</span>
+          <span class="p-3 bg-teal-lightest text-teal rounded shadow">G-Suite</span>
+         </div>
+
+         <h4 class="mb-4 text-xl font-medium">{{ __( 'website.skills.frameworks' ) }}</h4>
+         <div class="flex flex-wrap justify-around">
+          <span class="p-3 bg-red-lightest text-red rounded shadow">Laravel</span>
+          <span class="p-3 bg-red-lightest text-red rounded shadow">JQuery</span>
+          <span class="p-3 bg-red-lightest text-red rounded shadow">Vue</span>
+          <span class="p-3 bg-red-lightest text-red rounded shadow">Bootstrap/Bulma</span>
          </div>
         </div>
        </div>
