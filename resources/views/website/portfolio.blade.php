c98198087837d4a8a3554a6a5b053ac7dc8a5e45
Mindexperiment <andrewloyd@libero.it>
2019-02-20 15:23:38 +0100

    style portfolio section

--- resources/views/website/portfolio.blade.php
+++ resources/views/website/portfolio.blade.php
@@ -1,12 +1,12 @@
 <section id="portfolio">
- <div>
+ <div class="container mx-auto">
 
   <div>  
    <div class="flex">
 
-    <div class="flex-1">
+    <div class="flex-1 w-1/3 p-2">
      <figure>
-      <img src="{{ asset( 'images/layout/cmigea.jpg' ) }}" />
+      <img src="{{ asset( 'images/layout/cmigea.jpg' ) }}" class="max-w-full h-auto" />
       <figcaption>
        <h1>Centro Medico IGEA</h1>
        <a href="http://www.centromedigea.it" target="_blank">
@@ -18,9 +18,9 @@
      </figure>
     </div>
 
-    <div class="flex-1">
+    <div class="flex-1 w-1/3 p-2">
      <figure>
-      <img src="{{ asset( 'images/layout/sartori.jpg' ) }}" />
+      <img src="{{ asset( 'images/layout/sartori.jpg' ) }}" class="max-w-full h-auto" />
       <figcaption>
        <h1>Sartori Video su Misura</h1>
        <a href="https://www.sartorisilvia.com" target="_blank">
@@ -32,9 +32,9 @@
      </figure>
     </div>
 
-    <div class="flex-1">
+    <div class="flex-1 w-1/3 p-2">
      <figure>
-      <img src="{{ asset( 'images/layout/kaleskop.jpg' ) }}" />
+      <img src="{{ asset( 'images/layout/kaleskop.jpg' ) }}" class="max-w-full h-auto" />
       <figcaption>
        <h1>Kaleskop Advertisement</h1>
        <a href="https://kaleskop.com" target="_blank">
