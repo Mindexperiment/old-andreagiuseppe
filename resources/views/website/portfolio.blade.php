9d9058608402658c6320b02376286075cc1fd3ae
Mindexperiment <andrewloyd@libero.it>
2019-02-20 15:33:32 +0100

    style portfolio media

--- resources/views/website/portfolio.blade.php
+++ resources/views/website/portfolio.blade.php
@@ -3,7 +3,7 @@
 
     <div class="p-2">
      <figure class="flex items-start">
-      <img src="{{ asset( 'images/layout/cmigea.jpg' ) }}" class="max-w-full h-auto mr-4" />
+      <img src="{{ asset( 'images/layout/cmigea.jpg' ) }}" class="max-w-full h-auto w-12 mr-4" />
       <figcaption class="flex-1">
        <h1>Centro Medico IGEA</h1>
        <a href="http://www.centromedigea.it" target="_blank">
@@ -16,7 +16,7 @@
 
     <div class="p-2">
      <figure class="flex items-start">
-      <img src="{{ asset( 'images/layout/sartori.jpg' ) }}" class="max-w-full h-auto mr-4" />
+      <img src="{{ asset( 'images/layout/sartori.jpg' ) }}" class="max-w-full h-auto w-12 mr-4" />
       <figcaption class="flex-1">
        <h1>Sartori Video su Misura</h1>
        <a href="https://www.sartorisilvia.com" target="_blank">
@@ -29,7 +29,7 @@
 
     <div class="p-2">
      <figure class="flex items-start">
-      <img src="{{ asset( 'images/layout/kaleskop.jpg' ) }}" class="max-w-full h-auto mr-4" />
+      <img src="{{ asset( 'images/layout/kaleskop.jpg' ) }}" class="max-w-full h-auto w-12 mr-4" />
       <figcaption class="flex-1">
        <h1>Kaleskop Advertisement</h1>
        <a href="https://kaleskop.com" target="_blank">
@@ -37,7 +37,6 @@
         <span><i></i></span>
        </a>
       </figcaption>
-      <div></div>
      </figure>
     </div>
 
