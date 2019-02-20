9bc496dd4654165737173f28ae9dcec1bda6b86a
Mindexperiment <andrewloyd@libero.it>
2019-02-20 15:32:21 +0100

    style portfolio section

--- resources/views/website/portfolio.blade.php
+++ resources/views/website/portfolio.blade.php
@@ -1,41 +1,36 @@
 <section id="portfolio">
  <div class="container mx-auto">
 
-  <div>  
-   <div class="flex">
-
-    <div class="flex-1 w-1/3 p-2">
-     <figure>
-      <img src="{{ asset( 'images/layout/cmigea.jpg' ) }}" class="max-w-full h-auto" />
-      <figcaption>
+    <div class="p-2">
+     <figure class="flex items-start">
+      <img src="{{ asset( 'images/layout/cmigea.jpg' ) }}" class="max-w-full h-auto mr-4" />
+      <figcaption class="flex-1">
        <h1>Centro Medico IGEA</h1>
        <a href="http://www.centromedigea.it" target="_blank">
         <span>{{ __( 'website.portfolio.button' ) }}</span>
         <span><i></i></span>
        </a>
       </figcaption>
-      <div></div>
      </figure>
     </div>
 
-    <div class="flex-1 w-1/3 p-2">
-     <figure>
-      <img src="{{ asset( 'images/layout/sartori.jpg' ) }}" class="max-w-full h-auto" />
-      <figcaption>
+    <div class="p-2">
+     <figure class="flex items-start">
+      <img src="{{ asset( 'images/layout/sartori.jpg' ) }}" class="max-w-full h-auto mr-4" />
+      <figcaption class="flex-1">
        <h1>Sartori Video su Misura</h1>
        <a href="https://www.sartorisilvia.com" target="_blank">
         <span>{{ __( 'website.portfolio.button' ) }}</span>
         <span><i></i></span>
        </a>
       </figcaption>
-      <div></div>
      </figure>
     </div>
 
-    <div class="flex-1 w-1/3 p-2">
-     <figure>
-      <img src="{{ asset( 'images/layout/kaleskop.jpg' ) }}" class="max-w-full h-auto" />
-      <figcaption>
+    <div class="p-2">
+     <figure class="flex items-start">
+      <img src="{{ asset( 'images/layout/kaleskop.jpg' ) }}" class="max-w-full h-auto mr-4" />
+      <figcaption class="flex-1">
        <h1>Kaleskop Advertisement</h1>
        <a href="https://kaleskop.com" target="_blank">
         <span>{{ __( 'website.portfolio.button' ) }}</span>
@@ -45,9 +40,6 @@
       <div></div>
      </figure>
     </div>
-
-   </div>
-  </div>
 
  </div>
 </section>
